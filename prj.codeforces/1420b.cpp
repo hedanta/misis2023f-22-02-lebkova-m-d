e6ff464b7e1868318d4175b453b45747f51743a4
Marina Lebkova <m.lebkova@gmail.com>
2023-10-26 19:52:05 +0300

    add: 1420b

---
+++ prj.codeforces/1420b.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <vector>
+
+int main() {
+  int tests;
+  std::cin >> tests;
+
+  while (tests--) {
+    long long arr_size;
+    std::cin >> arr_size;
+    std::vector<int> arr(arr_size, 0);
+    long long ans = 0;
+
+    for (int i = 0; i < arr_size; i++) {
+      std::cin >> arr[i];
+    }
+
+    for (int k = 32; k >= 1; k -= 1) {
+      long long cnt = 0;
+
+      for (int i = 0; i < arr_size; i += 1) {
+        if (arr[i] >= (1 << k) && arr[i] < (1 << (k + 1))) {
+          cnt += 1;
+        }
+      }
+
+      ans += cnt * (cnt - 1) / 2;
+    }
+    std::cout << ans << std::endl;
+  }
+}
