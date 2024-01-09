53a7403234cb763ab18796c08f73bf9ec57cfb42
Marina Lebkova <m.lebkova@gmail.com>
2024-01-09 15:26:13 +1100

    add: 0102b

---
+++ 0102b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+long long sum(std::string s) {
+  long long res = 0;
+
+  for (int i = 0; i < s.size(); i += 1) {
+    res += s[i] - '0';
+  }
+
+  return res;
+}
+
+int main() {
+  std::string str;
+  std::cin >> str;
+  
+  int cnt = 0;
+  
+  while(str.size() > 1) {
+    long long x = sum(str);
+    str = std::to_string(x);
+    cnt++;
+  }
+  
+  std::cout << cnt;
+
+  return 0;
+}
