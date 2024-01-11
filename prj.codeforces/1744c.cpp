e468996119aaa08d68256f257d0b08c3da2e2deb
Marina Lebkova <m.lebkova@gmail.com>
2024-01-11 21:53:54 +1100

    add: 1744c

---
+++ prj.codeforces/1744c.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int tests;
+  std::cin >> tests;
+
+  for (int t = 0; t < tests; t += 1) {
+    int n;
+    cin >> n;
+    char c;
+    std::cin >> c;
+    std::string s;
+    std::cin >> s;
+    s += s;
+    int j = 0, ans = 0, seconds = 0;
+
+    for (int i = 0; i < n; i += 1) {
+      while (s[j] != 'g' || i > j) {
+        j += 1;
+      }
+
+      if (s[i] == c) {
+        seconds = j - i;
+      }
+
+      if (seconds > ans) {
+        ans = seconds;
+      }
+    }
+
+    std::cout << ans << std::endl;
+
+  }
+
+  return 0;
+}
